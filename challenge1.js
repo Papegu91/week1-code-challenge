a0abfafece9a4fa90e6e5a3ba19d9826a118edc2
peter gudoi <petergudoi@gmail.com>
2023-06-19 11:18:01 +0300

    first commit

---
+++ challenge1.js
@@ -0,0 +1,24 @@
+function grade(){
+    let marks=parseInt(prompt("enter marks"))
+    if(marks>=0 && marks <=100){
+        if(marks>79 && marks<=100){
+            document.writeln("A <br>")
+        }else if(marks>=60 && marks<=79){
+            document.writeln("B <br>")
+        }else if(marks>= 50 && marks<=59){
+            document.writeln("C <br>");
+        }
+        else if(marks>= 40 && marks<=49){
+            document.writeln("D <br>");
+        }else{
+            document.writeln("E <br>");
+        }
+
+        
+        
+
+    }else{
+        return
+    }
+}
+grade()
